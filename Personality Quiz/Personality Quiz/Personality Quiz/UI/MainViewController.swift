583c9425a3d836965a4fdbd291560fa1937acbbc
key5771 <ksj57715@hanmail.net>
2021-03-15 17:02:50 +0900

    UI

---
+++ Personality Quiz/Personality Quiz/Personality Quiz/UI/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by 김기현 on 2021/03/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
--- Personality Quiz/Personality Quiz/Personality Quiz/UI/QuestionViewController.swift
+++ Personality Quiz/Personality Quiz/Personality Quiz/UI/QuestionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QuestionViewController.swift
 //  Personality Quiz
 //
 //  Created by 김기현 on 2021/03/15.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QuestionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
-
