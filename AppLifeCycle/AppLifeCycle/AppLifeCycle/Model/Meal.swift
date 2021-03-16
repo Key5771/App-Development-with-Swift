621b35b4eab5fb1642cc393126e2e7f12bd8a728
key5771 <ksj57715@hanmail.net>
2021-03-16 15:33:37 +0900

    Meal Struct & MealListTableViewController

---
+++ AppLifeCycle/AppLifeCycle/AppLifeCycle/Model/Meal.swift
@@ -0,0 +1,16 @@
+//
+//  Meal.swift
+//  AppLifeCycle
+//
+//  Created by 김기현 on 2021/03/16.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String
+    var photo: String
+    var notes: String
+    var rating: Int
+    var timestamp: Date
+}
---
+++ AppLifeCycle/AppLifeCycle/AppLifeCycle/ViewControllers/MealListTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MealListTableViewController.swift
+//  AppLifeCycle
+//
+//  Created by 김기현 on 2021/03/16.
+//
+
+import UIKit
+
+class MealListTableViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var meal: [Meal] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
