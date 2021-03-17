92e2448a07094709650b9411ff7e6bd0195667d8
key5771 <ksj57715@hanmail.net>
2021-03-17 11:22:48 +0900

    Create EmojiModel & EmojiTableViewController

---
+++ EmojiDictionary/EmojiDictionary/EmojiDictionary/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiDictionary
+//
+//  Created by 김기현 on 2021/03/17.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
