3c2b3c89ed0bc15c2e9c89ac4ba0c674902ca875
key5771 <ksj57715@hanmail.net>
2021-03-17 15:40:12 +0900

    Emoji Dictionary Table View

---
+++ EmojiDictionary/EmojiDictionary/EmojiDictionary/UI/Cell/EmojiTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by 김기현 on 2021/03/17.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
