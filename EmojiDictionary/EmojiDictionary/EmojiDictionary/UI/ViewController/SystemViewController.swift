e1b3af71e5dca8a7e5646d16f99c7d371de892dd
key5771 <ksj57715@hanmail.net>
2021-03-22 15:33:54 +0900

    Safari & ImagePicker(Alert)

--- EmojiDictionary/EmojiDictionary/EmojiDictionary/UI/ViewController/SystemViewController.swift
+++ EmojiDictionary/EmojiDictionary/EmojiDictionary/UI/ViewController/SystemViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class SystemViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
@@ -24,12 +25,55 @@ class SystemViewController: UIViewController {
         self.present(activityController, animated: true, completion: nil)
     }
     
-    @IBAction func safariButtomTapped(_ sender: Any) {
+    @IBAction func safariButtomTapped(_ sender: UIButton) {
+        guard let url = URL(string: "https://www.apple.com") else { return }
+        
+        let safariViewController = SFSafariViewController(url: url)
+        present(safariViewController, animated: true, completion: nil)
     }
     
     @IBAction func cameraButtomTapped(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        cancelAction.setValue(UIColor.red, forKey: "titleTextColor")
+        alertController.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+                print("User selected Camera Action")
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            
+            alertController.addAction(cameraAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (action) in
+                print("User selected Photo Library Action")
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func emailButtonTapped(_ sender: Any) {
+    }
+}
+
+extension SystemViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        
+        imageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
     }
 }
