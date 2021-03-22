b68e297eb670f315ba11a0e041626ca02b39ff2c
key5771 <ksj57715@hanmail.net>
2021-03-22 10:50:39 +0900

    EmojiCollectionView Header Custom

---
+++ EmojiDictionary/EmojiDictionary/EmojiDictionary/UI/View/EmojiCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiCollectionReusableView.swift
+//  EmojiDictionary
+//
+//  Created by 김기현 on 2021/03/22.
+//
+
+import UIKit
+
+class EmojiCollectionReusableView: UICollectionReusableView {
+    @IBOutlet weak var headerTitleLabel: UILabel!
+    
+}
--- EmojiDictionary/EmojiDictionary/EmojiDictionary/UI/ViewController/EmojiCollectionViewController.swift
+++ EmojiDictionary/EmojiDictionary/EmojiDictionary/UI/ViewController/EmojiCollectionViewController.swift
@@ -10,15 +10,74 @@ import UIKit
 class EmojiCollectionViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var emojiTitle: [String] = ["Emoji", "Emoji2"]
+    var emojis: [Emoji] = [
+        Emoji(symbol: "😀",
+              name: "Grinning Face",
+              description: "A typical smiley face.",
+              usage: "happiness"),
+        Emoji(symbol: "😕",
+              name: "Confused Face",
+              description: "A confused, puzzled face.",
+              usage: "unsure what to think; displeasure"),
+        Emoji(symbol: "😍",
+              name: "Heart Eyes",
+              description: "A smiley face with hearts for eyes.",
+              usage: "love of something; attractive"),
+        Emoji(symbol: "👮",
+              name: "Police Officer",
+              description: "A police officer wearing a blue cap with a gold badge.",
+              usage: "person of authority"),
+        Emoji(symbol: "🐢",
+              name: "Turtle",
+              description: "A cute turtle.",
+              usage: "Something slow"),
+        Emoji(symbol: "🐘",
+              name: "Elephant",
+              description: "A gray elephant.",
+              usage: "good memory")
+    ]
+    
+    var emojis2: [Emoji] = [
+        Emoji(symbol: "🍝",
+              name: "Spaghetti",
+              description: "A plate of spaghetti.",
+              usage: "spaghetti"),
+        Emoji(symbol: "🎲",
+              name: "Die",
+              description: "A single die.",
+              usage: "taking a risk, chance; game"),
+        Emoji(symbol: "⛺️",
+              name: "Tent",
+              description: "A small tent.",
+              usage: "camping"),
+        Emoji(symbol: "📚",
+              name: "Stack of Books",
+              description: "Three colored books stacked on each other.",
+              usage: "homework, studying"),
+        Emoji(symbol: "💔",
+              name: "Broken Heart",
+              description: "A red, broken heart.",
+              usage: "extreme sadness"),
+        Emoji(symbol: "💤",
+              name: "Snore",
+              description: "Three blue \'z\'s.",
+              usage: "tired, sleepiness"),
+        Emoji(symbol: "🏁",
+              name: "Checkered Flag",
+              description: "A black-and-white checkered flag.",
+              usage: "completion")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        collectionView.register(UICollectionReusableView.self,
-                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                withReuseIdentifier: "header")
+//        collectionView.register(UICollectionReusableView.self,
+//                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+//                                withReuseIdentifier: "header")
         collectionView.register(UICollectionReusableView.self,
                                 forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
                                 withReuseIdentifier: "footer")
@@ -34,14 +93,14 @@ extension EmojiCollectionViewController: UICollectionViewDelegate {
 
 extension EmojiCollectionViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return emojiTitle.count
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if section == 0 {
-            return 4
+            return emojis.count
         } else {
-            return 5
+            return emojis2.count
         }
     }
     
@@ -60,12 +119,21 @@ extension EmojiCollectionViewController: UICollectionViewDelegateFlowLayout {
     //MARK: Header & Footer
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         if kind == UICollectionView.elementKindSectionHeader {
-            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
-            headerView.backgroundColor = .red
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) as? EmojiCollectionReusableView else {
+                return UICollectionReusableView()
+            }
+            
+//            headerView.backgroundColor = .red
+            if indexPath.section == 0 {
+                headerView.headerTitleLabel.text = emojiTitle[0]
+            } else {
+                headerView.headerTitleLabel.text = emojiTitle[1]
+            }
+            
             return headerView
         } else {
             let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "footer", for: indexPath)
-            footerView.backgroundColor = .purple
+            footerView.backgroundColor = .lightGray
             return footerView
         }
     }
@@ -75,7 +143,7 @@ extension EmojiCollectionViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return CGSize(width: self.view.frame.width, height: 50)
+        return CGSize(width: self.view.frame.width, height: 15)
     }
     
     // MARK: Cell
