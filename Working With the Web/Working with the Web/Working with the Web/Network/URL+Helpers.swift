dadbdb83f6b6c51df1eab8dc9b4649b7c1ad8d06
key5771 <ksj57715@hanmail.net>
2021-03-23 14:30:03 +0900

    Create PhotoInfoController, PhotoInfo, URL+Helpers

---
+++ Working With the Web/Working with the Web/Working with the Web/Network/URL+Helpers.swift
@@ -0,0 +1,16 @@
+//
+//  URL+Helpers.swift
+//  Working with the Web
+//
+//  Created by 김기현 on 2021/03/23.
+//
+
+import Foundation
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.map { URLQueryItem(name: $0.key, value: $0.value) }
+        return components?.url
+    }
+}
---
+++ Working With the Web/Working with the Web/Working with the Web/UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Working with the Web
+//
+//  Created by 김기현 on 2021/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
