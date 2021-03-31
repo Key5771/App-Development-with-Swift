7ffd58eb682aeda6781c81b460bd3590685c3869
key5771 <ksj57715@hanmail.net>
2021-03-31 15:15:50 +0900

    SideMenuViewController Confirm UIViewControllerDelegate, UITableViewDataSource

---
+++ LastMission/WebViewPage/WebViewPage/UI/ViewControllers/SideMenuController/SideMenuViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SideMenuViewController.swift
+//  WebViewPage
+//
+//  Created by 김기현 on 2021/03/31.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let item = ["Gallery"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension SideMenuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension SideMenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SideCell", for: indexPath) as? SideMenuTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.textLabel?.text = item[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
