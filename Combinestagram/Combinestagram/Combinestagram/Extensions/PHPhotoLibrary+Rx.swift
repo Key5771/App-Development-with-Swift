98b8da6b987a45590209d5bcf2de988a68f6f50a
key5771 <ksj57715@hanmail.net>
2021-04-06 13:07:41 +0900

    사용자가 접근 권한을 부여한 경우와 아닌 경우를 나누어 처리

---
+++ Combinestagram/Combinestagram/Combinestagram/Extensions/PHPhotoLibrary+Rx.swift
@@ -0,0 +1,31 @@
+//
+//  PHPhotoLibrary+Rx.swift
+//  Combinestagram
+//
+//  Created by 김기현 on 2021/04/06.
+//
+
+import Foundation
+import Photos
+import RxSwift
+
+extension PHPhotoLibrary {
+    static var authorized: Observable<Bool> {
+        return Observable.create { observer in
+            DispatchQueue.main.async {
+                if authorizationStatus() == .authorized {
+                    observer.onNext(true)
+                    observer.onCompleted()
+                } else {
+                    observer.onNext(false)
+                    requestAuthorization { newStatus in
+                        observer.onNext(newStatus == .authorized)
+                        observer.onCompleted()
+                    }
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
