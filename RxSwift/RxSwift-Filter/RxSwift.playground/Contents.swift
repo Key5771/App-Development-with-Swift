23d2a6e2e494aa8ad73e9e078e4dedd46f0e6116
key5771 <ksj57715@hanmail.net>
2021-04-06 14:42:04 +0900

    Transforming Elements

--- RxSwift/RxSwift-Filter/RxSwift.playground/Contents.swift
+++ RxSwift/RxSwift-Filter/RxSwift.playground/Contents.swift
@@ -171,3 +171,45 @@ example(of: "distinctUntilChanged(_:)") {
         }
         .disposed(by: disposeBag)
 }
+
+
+example(of: "toArray") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("A", "B", "C")
+        .toArray()
+        .subscribe {
+            print($0)
+        }
+        .disposed(by: disposeBag)
+}
+
+example(of: "map") {
+    let disposeBag = DisposeBag()
+    
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+    
+    Observable<Int>.of(123, 4, 56)
+        .map {
+            formatter.string(from: NSNumber(value: $0)) ?? ""
+        }
+        .subscribe {
+            print($0)
+        }
+        .disposed(by: disposeBag)
+}
+
+example(of: "enumerated and map") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .enumerated()
+        .map { index, integer in
+            index > 2 ? integer * 2 : integer
+        }
+        .subscribe {
+            print($0)
+        }
+        .disposed(by: disposeBag)
+}
