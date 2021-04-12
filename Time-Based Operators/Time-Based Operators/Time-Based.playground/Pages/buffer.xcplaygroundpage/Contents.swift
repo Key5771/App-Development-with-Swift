129511394abd716e1fb603cb4260fd45f2303b83
key5771 <ksj57715@hanmail.net>
2021-04-12 16:45:51 +0900

    buffer 페이지 내의 replay 분리

--- Time-Based Operators/Time-Based Operators/Time-Based.playground/Pages/buffer.xcplaygroundpage/Contents.swift
+++ Time-Based Operators/Time-Based Operators/Time-Based.playground/Pages/buffer.xcplaygroundpage/Contents.swift
@@ -23,51 +23,6 @@ class TimelineView<E>: TimelineViewBase, ObserverType where E: CustomStringConve
     }
 }
 
-//let elementsPerSecond = 1
-//let maxElements = 5
-//let replayedElemets = 1
-//let replayDelay: TimeInterval = 3
-//
-//let sourceObservable = Observable<Int>.create { observer in
-//    var value = 1
-//    let timer = DispatchSource.timer(interval: 1.0 / Double(elementsPerSecond), queue: .main) {
-//        if value <= maxElements {
-//            observer.onNext(value)
-//            value += 1
-//        }
-//    }
-//
-//    return Disposables.create {
-//        timer.suspend()
-//    }
-//}
-////.replay(replayedElemets)
-//.replayAll()
-//
-//let sourceTimeline = TimelineView<Int>.make()
-//let replayedTimeline = TimelineView<Int>.make()
-//
-//let stack = UIStackView.makeVertical([
-//    UILabel.makeTitle("replay"),
-//    UILabel.make("Emit \(elementsPerSecond) per second:"),
-//    sourceTimeline,
-//    UILabel.make("Replay \(replayedElemets) after \(replayDelay) sec:"),
-//    replayedTimeline
-//])
-//
-//_ = sourceObservable.subscribe(sourceTimeline)
-//
-//DispatchQueue.main.asyncAfter(deadline: .now() + replayDelay) {
-//    _ = sourceObservable.subscribe(replayedTimeline)
-//}
-//
-//_ = sourceObservable.connect()
-//
-//let hostView = setupHostView()
-//hostView.addSubview(stack)
-//hostView
-
-
 let bufferTimeSpan: RxTimeInterval = DispatchTimeInterval.seconds(4)
 let bufferMaxCount = 2
 
