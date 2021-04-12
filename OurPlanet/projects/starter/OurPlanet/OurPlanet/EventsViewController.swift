90e7f3eba1b0ae64e99490eb608432419f1e72d2
key5771 <ksj57715@hanmail.net>
2021-04-12 10:21:03 +0900

    Slider의 값에 따른 데이터 변화 적용

--- OurPlanet/projects/starter/OurPlanet/OurPlanet/EventsViewController.swift
+++ OurPlanet/projects/starter/OurPlanet/OurPlanet/EventsViewController.swift
@@ -41,6 +41,8 @@ class EventsViewController: UIViewController, UITableViewDataSource {
   @IBOutlet var daysLabel: UILabel!
   
   let events = BehaviorRelay<[EOEvent]>(value: [])
+  let days = BehaviorRelay<Int>(value: 360)
+  let filteredEvents = BehaviorRelay<[EOEvent]>(value: [])
   let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
@@ -49,27 +51,48 @@ class EventsViewController: UIViewController, UITableViewDataSource {
     tableView.rowHeight = UITableView.automaticDimension
     tableView.estimatedRowHeight = 60
     
-    events.asObservable()                                     // 이벤트가 새 값을 가져올 때마다 tableView를 업데이트
+    filteredEvents.asObservable()                                     // 이벤트가 새 값을 가져올 때마다 tableView를 업데이트
       .subscribe(onNext: { [weak self] _ in
         DispatchQueue.main.async {
           self?.tableView.reloadData()
         }
       })
       .disposed(by: disposeBag)
+    
+    Observable.combineLatest(days, events) { days, events -> [EOEvent] in
+      let maxInterval = TimeInterval(days * 24 * 3600)
+      
+      return events.filter { event in
+        if let date = event.date {
+          return abs(date.timeIntervalSinceNow ) < maxInterval
+        }
+        return true
+      }
+    }
+    .bind(to: filteredEvents)
+    .disposed(by: disposeBag)
+    
+    days.asObservable()
+      .subscribe(onNext: { [weak self] days in
+        self?.daysLabel.text = "Last \(days) days"
+      })
+      .disposed(by: disposeBag)
   }
 
   @IBAction func sliderAction(slider: UISlider) {
+    days.accept(Int(slider.value))
+    
   }
 
   // MARK: UITableViewDataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return events.value.count
+    return filteredEvents.value.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventCell
     
-    let event = events.value[indexPath.row]
+    let event = filteredEvents.value[indexPath.row]
     cell.configure(event: event)
     
     return cell
