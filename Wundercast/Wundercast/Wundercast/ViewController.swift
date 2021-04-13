b67b711473b18806c5f65f166a3e22177537299f
key5771 <ksj57715@hanmail.net>
2021-04-13 11:23:13 +0900

    기존 프로젝트에서 Seoul을 기본으로 두고 도시를 검색할 수 있도록 설정

--- Wundercast/Wundercast/Wundercast/ViewController.swift
+++ Wundercast/Wundercast/Wundercast/ViewController.swift
@@ -35,48 +35,75 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-  @IBOutlet private var searchCityName: UITextField!
-  @IBOutlet private var tempLabel: UILabel!
-  @IBOutlet private var humidityLabel: UILabel!
-  @IBOutlet private var iconLabel: UILabel!
-  @IBOutlet private var cityNameLabel: UILabel!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-
-    style()
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-  }
-
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-
-    Appearance.applyBottomLine(to: searchCityName)
-  }
-
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  // MARK: - Style
-
-  private func style() {
-    view.backgroundColor = UIColor.aztec
-    searchCityName.attributedPlaceholder = NSAttributedString(string: "City's Name",
-                                                              attributes: [.foregroundColor: UIColor.textGrey])
-    searchCityName.textColor = UIColor.ufoGreen
-    tempLabel.textColor = UIColor.cream
-    humidityLabel.textColor = UIColor.cream
-    iconLabel.textColor = UIColor.cream
-    cityNameLabel.textColor = UIColor.cream
-  }
+    @IBOutlet private var searchCityName: UITextField!
+    @IBOutlet private var tempLabel: UILabel!
+    @IBOutlet private var humidityLabel: UILabel!
+    @IBOutlet private var iconLabel: UILabel!
+    @IBOutlet private var cityNameLabel: UILabel!
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        style()
+        
+        ApiController.shared.currentWeather(for: "Seoul")
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { data in
+                self.tempLabel.text = "\(data.temperature)° C"
+                self.iconLabel.text = data.icon
+                self.humidityLabel.text = "\(data.humidity)%"
+                self.cityNameLabel.text = data.cityName
+            })
+            .disposed(by: disposeBag)
+        
+        searchCityName.rx.text.orEmpty
+            .filter { !$0.isEmpty }
+            .flatMap { text in
+                return ApiController.shared.currentWeather(for: text)
+                    .catchErrorJustReturn(ApiController.Weather.empty)
+            }
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { data in
+                self.tempLabel.text = "\(data.temperature)° C"
+                self.iconLabel.text = data.icon
+                self.humidityLabel.text = "\(data.humidity)"
+                self.cityNameLabel.text = data.cityName
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        Appearance.applyBottomLine(to: searchCityName)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Style
+    
+    private func style() {
+        view.backgroundColor = UIColor.aztec
+        searchCityName.attributedPlaceholder = NSAttributedString(string: "City's Name",
+                                                                  attributes: [.foregroundColor: UIColor.textGrey])
+        searchCityName.textColor = UIColor.ufoGreen
+        tempLabel.textColor = UIColor.cream
+        humidityLabel.textColor = UIColor.cream
+        iconLabel.textColor = UIColor.cream
+        cityNameLabel.textColor = UIColor.cream
+    }
 }
