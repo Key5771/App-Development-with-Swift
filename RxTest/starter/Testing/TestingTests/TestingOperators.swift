8ea99da7cf2a7435b9d85bc490f4bffe92966454
key5771 <ksj57715@hanmail.net>
2021-04-15 16:55:15 +0900

    RxTest -> testAmb(), testFilter()

--- RxTest/starter/Testing/TestingTests/TestingOperators.swift
+++ RxTest/starter/Testing/TestingTests/TestingOperators.swift
@@ -33,17 +33,87 @@
 import XCTest
 import RxSwift
 import RxTest
-import RxBlocking
 
 class TestingOperators : XCTestCase {
-  var scheduler: TestScheduler!
-  var subscription: Disposable!
-
-  override func setUp() {
-    super.setUp()
-  }
-
-  override func tearDown() {
-    super.tearDown()
-  }
+    var scheduler: TestScheduler!               // 각 테스트에서 사용할 TestScheduler의 인스턴스
+    var subscription: Disposable!               // 각 테스트에서 구독을 유지
+    
+    /*
+     각 테스트 케이스가 시작되기 전에 호출
+     */
+    override func setUp() {
+        super.setUp()
+        
+        scheduler = TestScheduler(initialClock: 0)
+    }
+    
+    /*
+     각 테스트가 완료될 때 호출
+     */
+    override func tearDown() {
+        scheduler.scheduleAt(1000) {            // 1000개의 가상 시간 단위로 테스트 구독 폐기를 예약
+            self.subscription.dispose()
+        }
+        
+        super.tearDown()
+    }
+    
+    /*
+     XCTest를 사용하는 모든 테스트와 마찬가지로 메소드 이름은 test로 시작
+     */
+    func testAmb() {
+        let observer = scheduler.createObserver(String.self)        // createObserver(_:) 메소드를 사용하여 observer를 생성
+        
+        // TestableObservable
+        let observableA = scheduler.createHotObservable([
+            Recorded.next(100, "a"),
+            Recorded.next(200, "b"),
+            Recorded.next(300, "c")
+        ])
+        
+        // TestableObservable
+        let observableB = scheduler.createHotObservable([
+            Recorded.next(90, "1"),
+            Recorded.next(200, "2"),
+            Recorded.next(300, "3")
+        ])
+        
+        let ambObservable = observableA.amb(observableB)
+        self.subscription = ambObservable.subscribe(observer)
+        scheduler.start()
+        
+        let results = observer.events.compactMap {
+            $0.value.element
+        }
+        
+        XCTAssertEqual(results, ["1", "2", "3"])
+    }
+    
+    func testFilter() {
+        let observer = scheduler.createObserver(Int.self)
+        
+        let observable = scheduler.createHotObservable([
+            Recorded.next(100, 1),
+            Recorded.next(200, 2),
+            Recorded.next(300, 3),
+            Recorded.next(400, 2),
+            Recorded.next(500, 1)
+        ])
+        
+        let filterObservable = observable.filter {
+            $0 < 3
+        }
+        
+        scheduler.scheduleAt(0) {
+            self.subscription = filterObservable.subscribe(observer)
+        }
+        
+        scheduler.start()
+        
+        let results = observer.events.compactMap {
+            $0.value.element
+        }
+        
+        XCTAssertEqual(results, [1, 2, 2, 1])
+    }
 }
