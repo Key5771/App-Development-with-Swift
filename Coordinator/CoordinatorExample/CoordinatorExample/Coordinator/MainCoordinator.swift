dc5d7dee539474b243e1b7599c0a7b43bd42ea0f
key5771 <ksj57715@hanmail.net>
2021-04-20 11:28:28 +0900

    BuyCoordinator로 BuyViewController를 관리하면서 MainCoordinator의 childCoordinators를 관리하는 코드 추가

--- Coordinator/CoordinatorExample/CoordinatorExample/Coordinator/MainCoordinator.swift
+++ Coordinator/CoordinatorExample/CoordinatorExample/Coordinator/MainCoordinator.swift
@@ -33,9 +33,28 @@ class MainCoordinator: NSObject, Coordinator {
         child.parentCoordinator = self
         childCoordinators.append(child)
         child.start()
+        
+        print("childCoordinators: \(childCoordinators)")
+    }
+    
+    func childDidFinish(_ child: Coordinator?) {
+        for (index, coordinator) in childCoordinators.enumerated() {
+            if coordinator === child {                                      // 두 인스턴스가 동일한 메모리를 가리키는지 점검
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
     }
 }
 
 extension MainCoordinator: UINavigationControllerDelegate {
-    
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else { return }
+        
+        if navigationController.viewControllers.contains(fromViewController) { return }
+        
+        if let buyViewController = fromViewController as? BuyViewController {
+            childDidFinish(buyViewController.coordinator)
+        }
+    }
 }
