f2947a31b525f374b7bf5a047bf272de357ec903
key5771 <ksj57715@hanmail.net>
2021-04-27 11:05:29 +0900

    Create Project - Target Version : iOS 9

---
+++ Practice/BeforeStart/BeforeStart/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BeforeStart
+//
+//  Created by 김기현 on 2021/04/27.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
